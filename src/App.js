5e267cdab9fe661db41484ff7a06560bf7a2a727
Alice Won <alicewon4@gmail.com>
2020-02-09 15:25:37 -0800

    homepagelayout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import FixedMenuLayout from './FixedMenuLayout'
+import HomepageLayout from './HomepageLayout'
+
 
 //state: isUserLogedIn: null
 
@@ -12,21 +15,8 @@ const notesURL= "http://localhost:3000/notes"
 class App extends React.Component {
   render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomepageLayout />
     </div>
   );
 }
