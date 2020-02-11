426eb934613213e32f90f33aa5b8a95dbaa88a56
Alice Won <alicewon4@gmail.com>
2020-02-10 16:40:14 -0800

    merge conflict 2 resolved

--- src/LoginSignupContainer.js
+++ src/LoginSignupContainer.js
@@ -1,30 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import LoginForm from './LoginForm'
-//import SignupForm from './SignupForm'
-
-
-//Callbacks:
-// setUser()
-// getUser()
-// createUser()
-// logUserOut()
-
-//render either login or signup form based on which one was clicked in the navbar
-
-class LoginSignupContainer extends React.Component{
-    render() {
-      return(
-        <>
-        <LoginForm onLogInUser={this.props.onLogInUser}/>
-       
-        </>
-      )
-    }
-=======
-=======
->>>>>>> Stashed changes
 // import { Button, Form } from 'semantic-ui-react'
 const itinitial_state = {
   user_name: ""
@@ -35,10 +9,6 @@ class LoginSignUpContainer extends React.Component {
     return(
       
     )
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
   }
 }
 
